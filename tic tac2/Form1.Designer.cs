2bb983dd56292ab6cda0515d23541d1f38c4643e
hashi93 <hashigalappaththi1993@gmail.com>
2015-06-28 13:32:06 +0530

    first commit

---
+++ tic tac2/Form1.Designer.cs
@@ -0,0 +1,157 @@
+﻿namespace tic_tac2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.R1a = new System.Windows.Forms.Button();
+            this.R2b = new System.Windows.Forms.Button();
+            this.R3b = new System.Windows.Forms.Button();
+            this.R3c = new System.Windows.Forms.Button();
+            this.R2c = new System.Windows.Forms.Button();
+            this.R1c = new System.Windows.Forms.Button();
+            this.R1b = new System.Windows.Forms.Button();
+            this.R3a = new System.Windows.Forms.Button();
+            this.R2a = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // R1a
+            // 
+            this.R1a.Location = new System.Drawing.Point(24, 53);
+            this.R1a.Name = "R1a";
+            this.R1a.Size = new System.Drawing.Size(75, 58);
+            this.R1a.TabIndex = 0;
+            this.R1a.UseVisualStyleBackColor = true;
+            this.R1a.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R2b
+            // 
+            this.R2b.Location = new System.Drawing.Point(105, 117);
+            this.R2b.Name = "R2b";
+            this.R2b.Size = new System.Drawing.Size(75, 58);
+            this.R2b.TabIndex = 1;
+            this.R2b.UseVisualStyleBackColor = true;
+            this.R2b.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R3b
+            // 
+            this.R3b.Location = new System.Drawing.Point(105, 181);
+            this.R3b.Name = "R3b";
+            this.R3b.Size = new System.Drawing.Size(75, 58);
+            this.R3b.TabIndex = 3;
+            this.R3b.UseVisualStyleBackColor = true;
+            this.R3b.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R3c
+            // 
+            this.R3c.Location = new System.Drawing.Point(186, 181);
+            this.R3c.Name = "R3c";
+            this.R3c.Size = new System.Drawing.Size(75, 58);
+            this.R3c.TabIndex = 4;
+            this.R3c.UseVisualStyleBackColor = true;
+            this.R3c.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R2c
+            // 
+            this.R2c.Location = new System.Drawing.Point(186, 117);
+            this.R2c.Name = "R2c";
+            this.R2c.Size = new System.Drawing.Size(75, 58);
+            this.R2c.TabIndex = 5;
+            this.R2c.UseVisualStyleBackColor = true;
+            this.R2c.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R1c
+            // 
+            this.R1c.Location = new System.Drawing.Point(186, 53);
+            this.R1c.Name = "R1c";
+            this.R1c.Size = new System.Drawing.Size(75, 58);
+            this.R1c.TabIndex = 6;
+            this.R1c.UseVisualStyleBackColor = true;
+            this.R1c.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R1b
+            // 
+            this.R1b.Location = new System.Drawing.Point(105, 53);
+            this.R1b.Name = "R1b";
+            this.R1b.Size = new System.Drawing.Size(75, 58);
+            this.R1b.TabIndex = 7;
+            this.R1b.UseVisualStyleBackColor = true;
+            this.R1b.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R3a
+            // 
+            this.R3a.Location = new System.Drawing.Point(24, 181);
+            this.R3a.Name = "R3a";
+            this.R3a.Size = new System.Drawing.Size(75, 58);
+            this.R3a.TabIndex = 8;
+            this.R3a.UseVisualStyleBackColor = true;
+            this.R3a.Click += new System.EventHandler(this.button_Click);
+            // 
+            // R2a
+            // 
+            this.R2a.Location = new System.Drawing.Point(24, 117);
+            this.R2a.Name = "R2a";
+            this.R2a.Size = new System.Drawing.Size(75, 58);
+            this.R2a.TabIndex = 9;
+            this.R2a.UseVisualStyleBackColor = true;
+            this.R2a.Click += new System.EventHandler(this.button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.R2a);
+            this.Controls.Add(this.R3a);
+            this.Controls.Add(this.R1b);
+            this.Controls.Add(this.R1c);
+            this.Controls.Add(this.R2c);
+            this.Controls.Add(this.R3c);
+            this.Controls.Add(this.R3b);
+            this.Controls.Add(this.R2b);
+            this.Controls.Add(this.R1a);
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+      
+        #endregion
+
+        private System.Windows.Forms.Button R1a;
+        private System.Windows.Forms.Button R2b;
+        private System.Windows.Forms.Button R3b;
+        private System.Windows.Forms.Button R3c;
+        private System.Windows.Forms.Button R2c;
+        private System.Windows.Forms.Button R1c;
+        private System.Windows.Forms.Button R1b;
+        private System.Windows.Forms.Button R3a;
+        private System.Windows.Forms.Button R2a;
+    }
+}
+
---
+++ tic tac2/Form1.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace tic_tac2
+{
+    public partial class Form1 : Form
+    {
+       // private bool turn;
+        private int count;
+        public Form1()
+        {
+            InitializeComponent();
+            count = 0;
+          //  turn = true;
+        }
+
+        private void button_Click(object sender, EventArgs e)
+        {
+            if (count <=9)
+            {
+                Button button = (Button)sender;
+                player_move(button);
+            }
+
+           
+            
+        }
+
+        private void player_move(Button button)
+        {
+          
+                button.Text = "X";
+                count++;
+           
+
+                AI_move();
+                count++;
+               // turn = true;
+         
+        }
+
+        private void AI_move()
+        {
+            bool move = false;
+           
+            move=Search_for_win("O");
+            if (move == false)
+            {
+                move = search_for_defeat("X");
+
+
+                if (move==false)
+                {
+                    move = search_for_coner();
+
+                    if (move==false)
+                    {
+                        move = search_for_free_cell();
+                    }
+
+                }
+            }
+            
+           
+
+        }
+
+        private bool Search_for_win(string text)
+        {
+            
+            //horizontal
+            if (R1a.Text == text && R1b.Text ==text && R1c.Text == "")
+                R1c.Text = text;
+            else if (R1a.Text == "" && R1b.Text == text && R1c.Text == text)
+                R1a.Text =text;
+            else if (R1a.Text == text && R1b.Text == "" && R1c.Text == text)
+                R1b.Text = text;
+
+            else if (R2a.Text == text && R2b.Text == text && R2c.Text == "")
+                R2c.Text =text;
+            else if (R2a.Text == "" && R2b.Text ==text && R2c.Text == text)
+                R2a.Text = text;
+            else if (R2a.Text == text && R2b.Text == "" && R2c.Text ==text)
+                R2b.Text = text;
+
+            else if (R3a.Text ==text && R3b.Text == text && R3c.Text == "")
+                R3c.Text = text;
+            else if (R3a.Text == "" && R3b.Text ==text && R3c.Text == text)
+                R3a.Text = text;
+            else if (R3a.Text == text && R3b.Text == "" && R3c.Text == text)
+                R3b.Text = text;
+
+
+            //vertical
+
+            else if (R1a.Text == text && R2a.Text == text && R3a.Text == "")
+                R3a.Text = text;
+            else if (R1a.Text == "" && R2a.Text ==text && R3a.Text == text)
+                R1a.Text = text;
+            else if (R1a.Text == text && R2a.Text == "" && R3a.Text == text)
+                R2a.Text = text;
+
+            else if (R1b.Text == text && R2b.Text == text && R3b.Text == "")
+                R3b.Text = text;
+            else if (R1b.Text == "" && R2b.Text == text && R3b.Text == text)
+                R1b.Text = text;
+            else if (R1b.Text == text && R2b.Text == "" && R3b.Text == text)
+                R2b.Text = text;
+
+            else if (R1c.Text == text && R2c.Text == text && R3c.Text == "")
+                R3c.Text = text;
+            else if (R1c.Text == "" && R2c.Text == text && R3c.Text == text)
+                R1c.Text = text;
+            else if (R1c.Text == text && R2c.Text == "" && R3c.Text == text)
+                R2c.Text = text;
+
+            //diagonal
+            else if (R1a.Text == text && R2b.Text == text && R3c.Text == "")
+                R3c.Text = text;
+            else if (R1a.Text == "" && R2b.Text == text && R3c.Text == text)
+                R1a.Text = text;
+            else if (R1a.Text == text && R2b.Text == "" && R3c.Text == text)
+                R2b.Text = text;
+
+            else if (R1c.Text == text && R2b.Text == text && R3a.Text == "")
+                R3a.Text = text;
+            else if (R1c.Text == "" && R2b.Text == text && R3a.Text == text)
+                R1c.Text = text;
+            else if (R1c.Text == text && R2b.Text == "" && R3a.Text == text)
+                R2b.Text = text;
+
+            else
+                return false;
+
+
+
+            return true;
+
+           
+
+        }
+
+        private bool search_for_coner()  
+        {
+            if (R1a.Text == "") { R1a.Text="O"; return true;}
+            else if(R1c.Text==""){R1c.Text="O"; return true;}
+            else if (R3a.Text == "") { R3a.Text = "O"; return true; }
+            else if (R3c.Text == "") { R3c.Text = "O"; return true; }
+            return false;
+        }
+
+        private bool search_for_defeat(string text)
+        {
+            //horizontal
+            if (R1a.Text == text && R1b.Text == text && R1c.Text == "")
+                R1c.Text = "O";
+            else if (R1a.Text == "" && R1b.Text == text && R1c.Text == text)
+                R1a.Text = "O";
+            else if (R1a.Text == text && R1b.Text == "" && R1c.Text == text)
+                R1b.Text = "O";
+
+            else if (R2a.Text == text && R2b.Text == text && R2c.Text == "")
+                R2c.Text = "O";
+            else if (R2a.Text == "" && R2b.Text == text && R2c.Text == text)
+                R2a.Text = "O";
+            else if (R2a.Text == text && R2b.Text == "" && R2c.Text == text)
+                R2b.Text = "O";
+
+            else if (R3a.Text == text && R3b.Text == text && R3c.Text == "")
+                R3c.Text = "O";
+            else if (R3a.Text == "" && R3b.Text == text && R3c.Text == text)
+                R3a.Text = "O";
+            else if (R3a.Text == text && R3b.Text == "" && R3c.Text == text)
+                R3b.Text = "O";
+
+
+            //vertical
+
+            else if (R1a.Text == text && R2a.Text == text && R3a.Text == "")
+                R3a.Text = "O";
+            else if (R1a.Text == "" && R2a.Text == text && R3a.Text == text)
+                R1a.Text = "O";
+            else if (R1a.Text == text && R2a.Text == "" && R3a.Text == text)
+                R2a.Text = "O";
+
+            else if (R1b.Text == text && R2b.Text == text && R3b.Text == "")
+                R3b.Text = "O";
+            else if (R1b.Text == "" && R2b.Text == text && R3b.Text == text)
+                R1b.Text = "O";
+            else if (R1b.Text == text && R2b.Text == "" && R3b.Text == text)
+                R2b.Text = "O";
+
+            else if (R1c.Text == text && R2c.Text == text && R3c.Text == "")
+                R3c.Text = "O";
+            else if (R1c.Text == "" && R2c.Text == text && R3c.Text == text)
+                R1c.Text = "O";
+            else if (R1c.Text == text && R2c.Text == "" && R3c.Text == text)
+                R2c.Text ="O";
+
+            //diagonal
+            else if (R1a.Text == text && R2b.Text == text && R3c.Text == "")
+                R3c.Text = "O";
+            else if (R1a.Text == "" && R2b.Text == text && R3c.Text == text)
+                R1a.Text = "O";
+            else if (R1a.Text == text && R2b.Text == "" && R3c.Text == text)
+                R2b.Text = "O";
+
+            else if (R1c.Text == text && R2b.Text == text && R3a.Text == "")
+                R3a.Text = "O";
+            else if (R1c.Text == "" && R2b.Text == text && R3a.Text == text)
+                R1c.Text = "O";
+            else if (R1c.Text == text && R2b.Text == "" && R3a.Text == text)
+                R2b.Text = "O";
+
+            else
+                return false;
+
+
+
+            return true; 
+        }
+
+        private bool search_for_free_cell()
+        {
+            Button b = null;
+            foreach (Control c in Controls)
+            {
+                b = c as Button;
+                if (b != null)
+                {
+                    if (b.Text == "")
+                    {
+                        b.Text = "O";
+                        return true;
+                    }
+                }
+                return false;
+            }
+
+
+            return true;
+        }
+
+    
+       
+
+       
+    }
+}
